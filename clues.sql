6113c6e3cb2760a371bce18653d395ba70165058
CLiu <callumliu09@hotmail.co.uk>
2025-05-29 08:53:38 +0100

    Clue #7 done

--- clues.sql
+++ clues.sql
@@ -31,6 +31,9 @@ SELECT name FROM cities WHERE countrycode = 'SMR' AND name != 'San Marino';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT name FROM cities WHERE name LIKE 'Serra%';
+SELECT * FROM cities WHERE name = 'Serra';
+SELECT * FROM countries WHERE code = 'BRA';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -38,7 +41,7 @@ SELECT name FROM cities WHERE countrycode = 'SMR' AND name != 'San Marino';
 -- follow right behind you!
 
 -- Write SQL query here
-
+SELECT * FROM cities WHERE id = 211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -52,3 +55,6 @@ SELECT name FROM cities WHERE countrycode = 'SMR' AND name != 'San Marino';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT * FROM cities WHERE population BETWEEN 91080 AND 91085;
+SELECT * FROM countries WHERE code = 'USA';
